a7e0fa9ea0791e5410a1495260dfe79ba8a42442
DinhTran77 <dinhtran77@hotmail.com>
2019-11-11 20:36:49 -0500

    Documentation

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Dinh's React drum machine
 
 ##Table of contents
 -Description
+-Installation
 -How to use
 -License
 -Author Info
@@ -12,4 +13,35 @@ Dinh's React drum machine
 ---
 
 ##Desciption
-A simple drum machin that plays
+A simple drum machine that can be played with mouse clicks or via the keyboard.
+
+---
+
+##Installation
+
+1.Download respository
+2.Install dependencies with "npm install"
+3.Start project with "npm start". Make sure you are in the project's root folder where package.json file is located.
+
+---
+
+##How to use
+
+Press the keypads with the mouse or use keyboard keys (Q, W, E, A, S, D, Z, X, C).
+
+---
+
+##License
+Copyright 2019. Dinh Tran
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+---
+
+##Author Info
+
+---
--- src/components/App.js
+++ src/components/App.js
@@ -1,103 +1,65 @@
-import React, { Component } from 'react';
-import DrumPad from './DrumPad';
-import './App.css';
+import React, { Component } from "react";
+import DrumPad from "./DrumPad";
+import "./App.css";
 
-const bankOne = [{
-  keyCode: 81,
-  keyTrigger: 'Q',
-  id: 'Heater-1',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-}, {
-  keyCode: 87,
-  keyTrigger: 'W',
-  id: 'Heater-2',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-}, {
-  keyCode: 69,
-  keyTrigger: 'E',
-  id: 'Heater-3',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-}, {
-  keyCode: 65,
-  keyTrigger: 'A',
-  id: 'Heater-4',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-}, {
-  keyCode: 83,
-  keyTrigger: 'S',
-  id: 'Clap',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-}, {
-  keyCode: 68,
-  keyTrigger: 'D',
-  id: 'Open-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-}, {
-  keyCode: 90,
-  keyTrigger: 'Z',
-  id: "Kick-n'-Hat",
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-}, {
-  keyCode: 88,
-  keyTrigger: 'X',
-  id: 'Kick',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-}, {
-  keyCode: 67,
-  keyTrigger: 'C',
-  id: 'Closed-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-},
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: "Chord 1 (Q)",
+    id: "Chord 1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "Chord 2 (W)",
+    id: "Chord 2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "Chord 3 (E)",
+    id: "Chord 3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "Rattle (A)",
+    id: "Rattle",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "Clap (S)",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "HH Open (D)",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "HH Kick (Z)",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "Kick (X)",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "HH Closed (C)",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
 ];
 
-const bankTwo = [{
-  keyCode: 81,
-  keyTrigger: 'Q',
-  id: 'Chord-1',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-}, {
-  keyCode: 87,
-  keyTrigger: 'W',
-  id: 'Chord-2',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-}, {
-  keyCode: 69,
-  keyTrigger: 'E',
-  id: 'Chord-3',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-}, {
-  keyCode: 65,
-  keyTrigger: 'A',
-  id: 'Shaker',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-}, {
-  keyCode: 83,
-  keyTrigger: 'S',
-  id: 'Open-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-}, {
-  keyCode: 68,
-  keyTrigger: 'D',
-  id: 'Closed-HH',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-}, {
-  keyCode: 90,
-  keyTrigger: 'Z',
-  id: 'Punchy-Kick',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-}, {
-  keyCode: 88,
-  keyTrigger: 'X',
-  id: 'Side-Stick',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-}, {
-  keyCode: 67,
-  keyTrigger: 'C',
-  id: 'Snare',
-  url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-}];
-
-const banks = [bankOne, bankTwo];
+const banks = [bankOne];
 
 const volumeHandlerStyle = {
   "--min": 0,
@@ -134,7 +96,7 @@ export default class App extends Component {
       bankIndex: 0,
       volumeValue: 50,
       displayText: this.props.displayTextDefault
-    }
+    };
 
     this.onBankChanged = this.onBankChanged.bind(this);
     this.onVolumeChanged = this.onVolumeChanged.bind(this);
@@ -163,15 +125,15 @@ export default class App extends Component {
     const iconVolumeElm = this.iconVolume.current;
     const displayVolumeValueElm = this.displayVolumeValue.current;
 
-    volumeHandlerElm.style.setProperty('--val', value);
+    volumeHandlerElm.style.setProperty("--val", value);
 
     if (value === 0) iconVolumeElm.className = "fas fa-volume-off";
     else if (value < 50) iconVolumeElm.className = "fas fa-volume-down";
     else iconVolumeElm.className = "fas fa-volume-up";
 
-    displayVolumeValueElm.style.setProperty('opacity', 1);
+    displayVolumeValueElm.style.setProperty("opacity", 1);
     setTimeout(() => {
-      displayVolumeValueElm.style.setProperty('opacity', 0);
+      displayVolumeValueElm.style.setProperty("opacity", 0);
     }, 1000);
 
     this.setState({
@@ -181,7 +143,7 @@ export default class App extends Component {
 
   onMouseLeaveInput() {
     setTimeout(() => {
-      this.displayVolumeValue.current.style.setProperty('opacity', 0);
+      this.displayVolumeValue.current.style.setProperty("opacity", 0);
     }, 1000);
   }
 
@@ -189,10 +151,9 @@ export default class App extends Component {
     if (this.hideVolumeTimeout) {
       clearTimeout(this.hideVolumeTimeout);
       this.hideVolumeTimeout = null;
-    }
-    else {
+    } else {
       this.hideVolumeTimeout = setTimeout(() => {
-        this.displayVolumeValue.current.style.setProperty('opacity', 0);
+        this.displayVolumeValue.current.style.setProperty("opacity", 0);
       }, 1000);
     }
   }
@@ -208,36 +169,51 @@ export default class App extends Component {
           <div className="drum-control-volumn">
             <i className="fas fa-volume-down" ref={this.iconVolume}></i>
             <span> Volume</span>
-            <span className="drum-control-volumn-value" ref={this.displayVolumeValue}> {this.state.volumeValue}</span>
-            <input type="range" onInput={this.onVolumeChanged} onMouseLeave={this.onMouseLeaveInput} style={volumeHandlerStyle} ref={this.volumeHandler} />
+            <span
+              className="drum-control-volumn-value"
+              ref={this.displayVolumeValue}
+            >
+              {" "}
+              {this.state.volumeValue}
+            </span>
+            <input
+              type="range"
+              onInput={this.onVolumeChanged}
+              onMouseLeave={this.onMouseLeaveInput}
+              style={volumeHandlerStyle}
+              ref={this.volumeHandler}
+            />
           </div>
-          <div className="drum-control-bank">
-            <div>{this.state.bankIndex ? "Smooth Piano Kit" : "Heater Kit"}</div>
+          {/* <div className="drum-control-bank">
+            <div>
+              {this.state.bankIndex ? "Smooth Piano Kit" : "Heater Kit"}
+            </div>
             <label className="switch" title="Switch bank">
-              <input type="checkbox" onChange={this.onBankChanged} ref={this.switchBank}/>
+              <input
+                type="checkbox"
+                onChange={this.onBankChanged}
+                ref={this.switchBank}
+              />
               <span className="slider round" ref={this.switchBankLabel}></span>
             </label>
-          </div>
+          </div> */}
         </div>
 
         <div className="drum-pads">
-          {
-            banks[this.state.bankIndex].map((item, idx) => {
-              return <DrumPad
+          {banks[this.state.bankIndex].map((item, idx) => {
+            return (
+              <DrumPad
                 key={idx}
                 padItem={item}
                 updateDisplayText={this.updateDisplayText}
-                volumeValue={this.state.volumeValue} />
-            })
-          }
+                volumeValue={this.state.volumeValue}
+              />
+            );
+          })}
         </div>
 
-        <div className="drum-footer">
-          
-        </div>
-
-        
+        <div className="drum-footer"></div>
       </div>
-    )
+    );
   }
 }
